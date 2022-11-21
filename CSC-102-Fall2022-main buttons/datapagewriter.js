a26069de6c5ff66b8e5b722f05f05e29f1bc1dc9
rarechro <paytonjanko@gmail.com>
2022-11-21 16:00:12 -0700

    functional data loader

--- CSC-102-Fall2022-main buttons/datapagewriter.js
+++ CSC-102-Fall2022-main buttons/datapagewriter.js
@@ -61,7 +61,7 @@ function getData(){
 function dataRow(legend,value,units){
 msg = "<td>";
 msg += legend;
-msg += "; </td><td>";
+msg += ": </td><td>";
 msg += value;
 msg += " " + units;
 msg += "</td>";
@@ -77,51 +77,79 @@ function updateDisplay(){
     theTime = new Date();
     console.log(theTime.getHours() + ":" + (theTime.getMinutes() < 10 ? "0" + theTime.getMinutes() : theTime.getMinutes())
     + ":" + (theTime.getSeconds() < 10 ? "0" + theTime.getSeconds : theTime.getSeconds()));
+//set clock element to current time and every 5 seconds update clock
+document.getElementById("clockTime").innerHTML = "The current time is: " + theTime.getHours() + ":" + (theTime.getMinutes() < 10 ? "0" + theTime.getMinutes() : theTime.getMinutes())
+    + ":" + (theTime.getSeconds() < 10 ? "0" + theTime.getSeconds : theTime.getSeconds());
 
-//update table
+//sets unique elements to data in dataloader.js and updates table
 var timeRead = data[index].time_sec;
 
 if (timeRead >= 10){
     document.getElementById("dataTable").rows["timeelapsed"].innerHTML = 
         dataRow("Time Elapsed",data[index].time_sec,"seconds");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-    document.getElementById("dataTable").rows["latitude"].innerHTML = 
-        dataRow("Latitude",data[index].latitude,"");
-}
 
+    document.getElementById("dataTable").rows["latitude"].innerHTML = 
+        dataRow("Latitude",data[index].latitude,"");
+
+    document.getElementById("dataTable").rows["longitude"].innerHTML = 
+        dataRow("Longitude",data[index].longitude,"");
+
+    document.getElementById("dataTable").rows["gpsalt"].innerHTML = 
+        dataRow("GPS Altitude",data[index].gps_alt,"");
+
+    document.getElementById("dataTable").rows["BMPsAlt"].innerHTML = 
+        dataRow("BMP Sensor Altitude",data[index].BMPsAlt,"");
+
+    document.getElementById("dataTable").rows["BMPsPres"].innerHTML = 
+        dataRow("BMP Sensor Pressure",data[index].BMPsPres,"");
+    
+    document.getElementById("dataTable").rows["BMPsTemp"].innerHTML = 
+        dataRow("BMP Sensor Temprature",data[index].BMPsTemp,"");
+
+    document.getElementById("dataTable").rows["DigsTemp"].innerHTML = 
+        dataRow("Digital Sensor Temprature",data[index].DIGsTemp,"");
+
+    document.getElementById("dataTable").rows["CSSsec02"].innerHTML = 
+        dataRow("CSS Sensor c02",data[index].CSSseC02,"");
+
+    document.getElementById("dataTable").rows["CSSsTVOC"].innerHTML = 
+        dataRow("CSS Sensor TVOC",data[index].CSSsTVOC,"");
+
+    document.getElementById("dataTable").rows["UV"].innerHTML = 
+        dataRow("UV",data[index].UV,"");
+
+    document.getElementById("dataTable").rows["AccelX"].innerHTML = 
+        dataRow("Accel X",data[index].accelX,"");
+
+    document.getElementById("dataTable").rows["AccelY"].innerHTML = 
+        dataRow("Accel Y",data[index].accelY,"");
+
+    document.getElementById("dataTable").rows["AccelZ"].innerHTML = 
+        dataRow("Accel Z",data[index].accelZ,"");
+
+    document.getElementById("dataTable").rows["MAGX"].innerHTML = 
+        dataRow("MAG X",data[index].magX,"");
+
+    document.getElementById("dataTable").rows["MAGY"].innerHTML = 
+        dataRow("MAG Y",data[index].magY,"");
+
+    document.getElementById("dataTable").rows["MAGZ"].innerHTML = 
+        dataRow("MAG Z",data[index].magZ,"");
+
+    document.getElementById("dataTable").rows["GYROX"].innerHTML = 
+        dataRow("GYRO X",data[index].gyrox,"");
+
+    document.getElementById("dataTable").rows["GYROY"].innerHTML = 
+        dataRow("GYRO Y",data[index].gyroy,"");
+    document.getElementById("dataTable").rows["GYROZ"].innerHTML = 
+        dataRow("GYRO Z",data[index].gyroz,"");
+
+}
+if (index < 500){
+    index++;
+
+}else{
+
+    index = 0;
+}
 }
